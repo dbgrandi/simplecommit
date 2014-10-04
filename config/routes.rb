f1b7baec57031f2638cb6533079146bb2dcf17db
David Grandinetti <dave@wegoto12.com>
2014-10-04 16:44:09 -0400

    added basic user model, and some oauth flows

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  get "/auth/:provider/callback" => "sessions#create"
+  get "/signout" => "sessions#destroy", :as => :signout
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
