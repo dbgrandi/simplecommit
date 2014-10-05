d9e5f823c034bef1382453eeab53faa9ae068a1f
David Grandinetti <dave@wegoto12.com>
2014-10-05 11:58:58 -0400

    add DatabaseCleaner to spec helper to manage transactions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,13 @@ require 'minitest/autorun'
 require 'rspec/rails'
 require 'rspec/autorun'
 
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
+
+# then, whenever you need to clean the DB
+DatabaseCleaner.clean
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
@@ -29,6 +36,20 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+  
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :transaction
+#      DatabaseCleaner.orm = "mongoid"
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
+  
 end
 
 
