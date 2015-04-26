ac195a7a5dcc59297b010e8622e70441ca5913b2
David Grandinetti <dbgrandi@yahoo-inc.com>
2015-04-26 16:06:16 -0400

    move coveralls init earlier in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,15 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+# enable code coverage
+require 'coveralls'
+Coveralls.wear!
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'minitest/autorun'
 require 'rspec/rails'
 require 'rspec/autorun'
 
 require 'database_cleaner'
-
-# enable code coverage
-require 'coveralls'
-Coveralls.wear!
 
 DatabaseCleaner.strategy = :truncation
 # then, whenever you need to clean the DB
